c7dfbe182ac07715cacdd26b56fedc73a0ff2bc2
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-07-30 00:14:48 +0200

    code added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
-<script></script>
+<script>
+</script>
 
-<nav class="flex flex-row items-center justify-between px-64 py-8 text-2xl text-blue-900">
+<nav class="flex flex-row items-center justify-between px-2 lg:px-64 py-8 text-2xl text-blue-900">
 	<div>
 		<h1 class="font-extrabold text-black">Mew</h1>
 	</div>
-	<div class="flex flex-row gap-16">
+	<div class="hidden md:flex lg:flex flex-row gap-16">
 		<a href="#home">Home</a>
 		<a href="#About">About</a>
 		<a href="#services">Services</a>
@@ -19,54 +20,68 @@
 	</div>
 </nav>
 
-<section class="py-32">
-	<img src="/paw.png" alt="paw icon of a dog" id="paw-image" />
-	<img src="/star.png" alt="star icon" id="star-image" />
-	<img src="/rings.png" alt="ring design for dierenkliniek" id="rings-image" />
-	<div class="flex flex-row gap-x-32">
-		<div class="relative">
+<section class="lg:py-32 lg:px-0 px-2">
+	<img src="/paw.png" alt="paw icon of a dog" id="paw-image" class="hidden lg:block" />
+	<img src="/star.png" alt="star icon" id="star-image" class="hidden lg:block" />
+	<div class="flex flex-col lg:flex-row gap-x-0 lg:gap-x-32">
+		<div class="hidden lg:block lg:relative">
 			<img src="/cathero.png" alt="" class="border-4 border-l-0 border-white rounded-3xl" />
 			<img src="/doghero.png" alt="" class="border-4 border-white rounded-3xl" id="doghero" />
 		</div>
-		<div class="flex flex-col justify-center w-2/4 gap-16">
+
+		<div class="lg:relative flex flex-col justify-center gap-4 lg:w-2/4 lg:gap-16">
 			<h1 class="text-6xl font-bold text-blue-900">We take care of your pet and help them grow</h1>
 			<h4 class="text-xl">
 				Best pets are waiting for adoption. Find out the perfect one you will like it. It's our
 				promise.
 			</h4>
-			<div class="flex flex-row items-center gap-4">
-				<button class="px-8 py-4 text-lg text-white bg-orange-600 rounded-2xl">Contact us</button>
-				<div>pictures of ...</div>
+			<div class="flex flex-row items-center gap-0 lg:gap-4">
+				<button
+					class="px-8 py-4 font-semibold text-white transition ease-in-out bg-orange-500 shadow-lg hover:scale-105 hover:bg-orange-400 hover:shadow-xl rounded-2xl"
+					>Contact us</button
+				>
 			</div>
+			<img
+				src="/rings.png"
+				alt="ring design for dierenkliniek"
+				id="rings-image"
+				class="hidden lg:block"
+			/>
 		</div>
 	</div>
 </section>
-<section class="my-32">
-	<div class="flex justify-center px-32 text-3xl">
+
+<section class="my-32 mx-2">
+	<div class="flex-col lg:flex-row flex justify-center lg:px-32 text-3xl gap-2 lg:gap-0">
 		<div
-			class="flex flex-col items-center content-center w-3/5 px-24 mt-16 text-white align-middle transition-transform bg-orange-600 rounded-bl-lg py-28 lg:flex-row rounded-tl-3xl bg-gradient-to-r to-orange-400 from-orange-500 hover:to-orange-500 hover:from-orange-400"
+			class="flex py-4 flex-col items-center content-center lg:w-3/5 lg:px-24 lg:mt-16 text-white align-middle bg-orange-600
+			rounded-tr-3xl lg:rounded-tr-none md:rounded-tr-none lg:rounded-bl-lg md:rounded-bl-lg
+			lg:py-28 lg:flex-row rounded-tl-3xl bg-gradient-to-r to-orange-400 from-orange-500"
 		>
-			<div class="flex flex-col items-center w-1/3 gap-4">
+			<div class="flex flex-col items-center lg:w-1/3 lg:gap-4">
 				<h3>Adopted</h3>
 				<h3 class="slackey">2500+</h3>
 			</div>
-			<div class="flex flex-col items-center w-1/3 gap-4 border-white border-x-4">
+			<div
+				class="flex flex-col items-center lg:w-1/3 lg:gap-4 border-white lg:border-y-0 md:border-y-0 border-y-2 lg:border-x-4"
+			>
 				<h3>Customers</h3>
 				<h3 class="slackey">1850+</h3>
 			</div>
-			<div class="flex flex-col items-center w-1/3 gap-4">
+			<div class="flex flex-col items-center lg:w-1/3 lg:gap-4">
 				<h3>Testimonials</h3>
 				<h3 class="slackey">1100+</h3>
 			</div>
 		</div>
 		<div
-			class="flex flex-col justify-center w-2/5 gap-4 px-8 text-white border-8 border-b-0 border-white bg-sky-400 rounded-t-3xl rounded-br-xl"
+			class="flex flex-col justify-center lg:p-0 lg:w-2/5 lg:gap-4 lg:px-8 text-white px-4 py-2
+			lg:border-t-8 md:border-t-8 lg:border-x-8 md: md:border-x-8 border-white from-sky-400 to-sky-600 bg-gradient-to-tr lg:rounded-t-3xl md:rounded-t-3xl rounded-t-none rounded-b-3xl lg:rounded-b-none md:rounded-b-none rounded-br-xl"
 		>
-			<p class="py-4 text-2xl border-b-2 border-white">
+			<p class="py-4 text-xl lg:text-2xl border-b-2 text-justify lg:text-left border-white">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eum voluptatem inventore a.
 				Eligendi molestias in, dicta dignissimos iure eveniet voluptatum itaque.
 			</p>
-			<div class="flex flex-row justify-between">
+			<div class="flex flex-row items-center justify-between py-2">
 				<div class="">
 					<h5 class="slackey">Arno De Bakker</h5>
 				</div>
@@ -76,18 +91,18 @@
 	</div>
 </section>
 
-<section class="px-32">
-	<div class="flex justify-between text-lg lg:flex-row">
-		<div class="flex flex-col w-1/2">
-			<h1 class="mb-8 text-5xl text-blue-900">Lets book a call and stay connected</h1>
-			<div class="flex flex-row gap-8 mb-8 font-semibold text-blue-950">
-				<div class="flex flex-row items-center gap-4">
+<section class="lg:px-32 lg:pb-32 mx-2 text-center lg:text-left">
+	<div class="flex flex-col justify-between text-lg lg:flex-row">
+		<div class="flex flex-col justify-center lg:w-1/2 gap-4 lg:gap-8">
+			<h1 class="text-5xl text-blue-900 mb-4 lg:mb-0">Lets book a call and stay connected</h1>
+			<div class="flex flex-col lg:flex-row gap-4 lg:gap-8 font-semibold text-blue-950">
+				<div class="flex flex-row items-center gap-4 justify-center">
 					<div>
 						<img src="/check.png" alt="" />
 					</div>
 					<div>Vaccination Service</div>
 				</div>
-				<div class="flex flex-row items-center gap-4">
+				<div class="flex flex-row items-center gap-4 justify-center">
 					<div>
 						<img src="/check.png" alt="" />
 					</div>
@@ -99,25 +114,296 @@
 				labore et dolore magna aliqua. Ut enim ad minim velit esse cillum dolore eu fugiat nulla
 				pariatur.
 			</p>
-			<div class="flex flex-row">
+			<div class="flex flex-col lg:flex-row gap-x-4 gap-4 lg:gap-0">
 				<div>
 					<button
-						class="px-8 py-4 font-semibold text-white transition-transform bg-orange-500 rounded-2xl"
+						class="px-8 py-4 font-semibold text-white transition ease-in-out bg-orange-500 shadow-lg hover:scale-105 hover:bg-orange-400 hover:shadow-xl rounded-2xl"
 					>
 						<a href="/book">Book a Schedule</a>
 					</button>
 				</div>
-				<div class="flex flex-row">
-					<img src="/call.png" alt="bel nu voor ..." />
-					<p class="underline">Emergency Call</p>
+				<div
+					class="flex flex-row items-center content-center justify-center transition hover:scale-105"
+				>
+					<div>
+						<img src="/call.png" alt="bel nu voor ..." />
+					</div>
+					<div><a href="/call" class="underline">Emergency call</a></div>
 				</div>
 			</div>
 		</div>
 		<div>
-			<img src="/doc-holding-dog.png" alt="" />
+			<img src="/doc-holding-dog.png" class="mt-16 lg:mt-0" alt="" />
 		</div>
 	</div>
 </section>
+
+<section class="py-20 bg-orange-50 lg:px-0 md:px-0 px-2">
+	<div class="flex flex-row items-center content-center justify-center">
+		<img src="/paw.png" alt="" />
+		<h1 class="text-5xl slackey text-blue-950">Our Services</h1>
+	</div>
+	<p class="font-light text-center text-gray-500">
+		Best pets are waiting for adoption. Find out the perfect ...
+	</p>
+	<div class="flex flex-col justify-center gap-8 mt-16 text-center lg:flex-row lg:px-36">
+		<div class="px-8 text-white bg-orange-500 shadow-md rounded-3xl lg:mb-16">
+			<img src="/sringe.png" class="mx-auto my-8" alt="" />
+			<h3 class="mb-4 text-2xl underline slackey">Vaccination</h3>
+			<p class="mb-8">Perfect one you will ikte it. It our promiese.</p>
+		</div>
+		<div class="px-8 text-gray-800 bg-white shadow-md rounded-3xl lg:mt-16">
+			<img src="/dog-icon.png" class="mx-auto my-8" alt="" />
+			<h3 class="mb-4 text-2xl text-gray-700 slackey">Vaccination</h3>
+			<p class="mb-8">Perfect one you will ikte it. It our promiese.</p>
+		</div>
+		<div class="px-8 text-gray-800 bg-white shadow-md rounded-3xl lg:mb-16">
+			<img src="/doc-icon.png" class="mx-auto my-8" alt="" />
+			<h3 class="mb-4 text-2xl text-gray-700 slackey">Vaccination</h3>
+			<p class="mb-8">Perfect one you will ikte it. It our promiese.</p>
+		</div>
+		<div class="px-8 text-gray-800 bg-white shadow-md rounded-3xl lg:mt-16">
+			<img src="/hands-icon.png" class="mx-auto my-8" alt="" />
+			<h3 class="mb-4 text-2xl text-gray-700 slackey">Vaccination</h3>
+			<p class="mb-8">Perfect one you will ikte it. It our promiese.</p>
+		</div>
+	</div>
+	<div class="flex flex-row justify-center gap-8 mt-24">
+		<div>
+			<button
+				class="px-8 py-6 text-lg font-semibold text-white transition ease-in-out bg-orange-600 shadow-xl hover:scale-105 hover:bg-orange-400 hover:shadow-xl rounded-2xl"
+			>
+				<a href="/book">Book a Schedule</a>
+			</button>
+		</div>
+		<div
+			class="flex flex-row justify-center gap-4 my-8 transition-all duration-1000 hover:translate-x-16 hover:opacity-0"
+		>
+			<img src="/arrows/Vector3.png" alt="" />
+			<img src="/arrows/Vector2.png" alt="" />
+			<img src="/arrows/Vector1.png" alt="" />
+			<img src="/arrows/Vector.png" alt="" />
+		</div>
+	</div>
+</section>
+
+<section class="py-20 text-center">
+	<h1 class="mb-16 text-5xl text-center slackey text-blue-950">Faculties we provide</h1>
+	<div class="grid grid-cols-1 lg:grid-cols-3 lg:mx-20">
+		<div class="p-4 lg:border-b-4 border-orange-50 lg:text-left">
+			<h3 class="lg:mb-8 text-2xl slackey text-blue-950">Overnight Care</h3>
+			<p class="text-gray-800 lg:text-lg">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum repudiandae.
+			</p>
+		</div>
+		<div class="flex justify-center row-span-3 p-4 border-4 rounded-full border-orange-50">
+			<img src="/standing-dog.png" alt="" class="lg:p-24 rounded-full" />
+		</div>
+		<div class="p-4 lg:pb-16 lg:text-right lg:border-b-4 border-orange-50">
+			<h3 class="mb-8 text-2xl slackey text-blue-950">Overnight Care</h3>
+			<p class="text-gray-800 lg:text-lg">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum repudiandae.
+			</p>
+		</div>
+		<div class="relative p-4 lg:text-left lg:border-b-4 border-orange-50">
+			<h3 class="mt-8 mb-8 text-2xl slackey text-blue-950">Overnight Care</h3>
+			<p class="text-gray-800 lg:text-lg">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum repudiandae.
+			</p>
+			<div
+				class="hidden lg:block absolute w-10 h-10 border-4 rounded-full border-orange-50
+				-top-5 -right-5 from-orange-200 to-orange-500 bg-gradient-to-bl"
+			/>
+			<div
+				class="hidden lg:block absolute w-10 h-10 border-4 rounded-full border-orange-50
+				-bottom-5 -right-5 from-orange-200 to-orange-500 bg-gradient-to-bl"
+			/>
+		</div>
+		<div class="relative p-4 lg:py-16 lg:text-right lg:border-b-4 border-orange-50">
+			<h3 class="mb-8 text-2xl slackey text-blue-950">Overnight Care</h3>
+			<p class="text-gray-800 lg:text-lg">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum repudiandae.
+			</p>
+			<div
+				class="hidden lg:block absolute w-10 h-10 border-4 rounded-full border-orange-50 -top-5 -left-5 from-orange-200 to-orange-500 bg-gradient-to-bl"
+			/>
+			<div
+				class="hidden lg:block absolute w-10 h-10 border-4 rounded-full border-orange-50 -bottom-5 -left-5 from-orange-200 to-orange-500 bg-gradient-to-bl"
+			/>
+		</div>
+		<div class="lg:text-left p-4 lg:pt-16">
+			<h3 class="mb-8 text-2xl slackey text-blue-950">Overnight Care</h3>
+			<p class="text-gray-800 lg:text-lg">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum repudiandae.
+			</p>
+		</div>
+		<div class="p-4 lg:pt-16 lg:text-right">
+			<h3 class="mb-8 text-2xl slackey text-blue-950">Overnight Care</h3>
+			<p class="text-gray-800 lg:text-lg">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum repudiandae.
+			</p>
+		</div>
+	</div>
+</section>
+
+<!--
+<section class="relative pt-20 pb-40 bg-orange-50">
+	<h1 class="mb-16 text-5xl text-center slackey text-blue-950">Our Groomers</h1>
+	<img src="/rings.png" class="absolute scale-75 right-20 top-16" alt="" />
+	<img src="/blitz.png" class="absolute top-16 left-20" alt="" />
+	<div class="flex justify-center gap-8 mt-56 text-center lg:flex-row">
+		<div
+			class="relative transition-all bg-orange-100 rounded-2xl hover:scale-105 hover:bg-orange-200 border-gradi"
+		>
+			<img
+				src="/team/maz.png"
+				class="absolute border-8 border-white rounded-full -top-28 center-image"
+				alt=""
+			/>
+			<div class="flex flex-col gap-4 px-16 mx-4 mt-48 mb-8 justif-center">
+				<h5 class="font-bold text-blue-900">Mouran Maz</h5>
+				<h6 class="text-blue-950">Care Taker</h6>
+			</div>
+		</div>
+		<div
+			class="relative transition-all bg-orange-100 rounded-2xl hover:scale-105 hover:bg-orange-200"
+		>
+			<img
+				src="/team/adam.png"
+				class="absolute border-8 border-white rounded-full -top-28 center-image"
+				alt=""
+			/>
+			<div class="flex flex-col gap-4 px-16 mx-4 mt-48 mb-8 justif-center">
+				<h5 class="font-bold text-blue-900">Adam Smith</h5>
+				<h6 class="text-blue-950">Veterenian</h6>
+			</div>
+		</div>
+		<div
+			class="relative transition-all bg-orange-100 rounded-2xl hover:scale-105 hover:bg-orange-200"
+		>
+			<img
+				src="/team/ana.png"
+				class="absolute border-8 border-white rounded-full -top-28 center-image"
+				alt=""
+			/>
+			<div class="flex flex-col gap-4 px-16 mx-4 mt-48 mb-8 justif-center">
+				<h5 class="font-bold text-blue-900">Sarah Jones</h5>
+				<h6 class="text-blue-950">Trainer</h6>
+			</div>
+		</div>
+		<div
+			class="relative transition-all bg-orange-100 rounded-2xl hover:scale-105 hover:bg-orange-200"
+		>
+			<img
+				src="/team/morgan.png"
+				class="absolute border-8 border-white rounded-full -top-28 center-image"
+				alt=""
+			/>
+			<div class="flex flex-col gap-4 px-16 mx-4 mt-48 mb-8 justif-center">
+				<h5 class="font-bold text-blue-900">Morgan Mark</h5>
+				<h6 class="text-blue-950">Trainer</h6>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="py-20 mx-32 my-16">
+	<div class="flex flex-row lg:flew-col px-lg:px-36">
+		<div class="flex-col gap-4 pr-20 lg:w-1/2">
+			<h1 class="mb-16 text-5xl slackey text-blue-950">Mew Blog</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo commodi, veniam libero vitae
+				suscipit blanditiis pariatur officia ea animi voluptatum repellendus. Distinctio nihil quas
+				deleniti. Sed eligendi veritatis dolor explicabo.
+			</p>
+			<button
+				class="px-8 py-4 mt-8 font-semibold text-white transition ease-in-out bg-orange-500 shadow-lg hover:scale-105 hover:bg-orange-400 hover:shadow-xl rounded-2xl"
+			>
+				<a href="/blog">Read now</a>
+			</button>
+		</div>
+		<div class="flex flex-row w-1/2 gap-4 p-4 rounded-md shadow-md bg-orange-50">
+			<div class="w-1/2 px-8 py-4 rounded-xl bg-sky-300">
+				<img src="puppy-blog.png" alt="" />
+			</div>
+			<div class="flex flex-col w-1/2">
+				<h4 class="mb-4 text-orange-900 lg:text-2xl slackey">How to keep your dog happy!</h4>
+				<p class="font-light text-gray-700">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi temporibus quasi sapiente
+					nobis a quos asperiores adipisci ea praesentium harum voluptatum cum, omnis, voluptates
+					consectetur ducimus recusandae iusto? Molestiae, facilis. Cupiditate enim maxime, placeat
+					vero rem error reprehenderit.
+				</p>
+				<div class="flex flex-row gap-2 m-4 mt-auto ml-auto">
+					<div
+						class="flex items-center justify-center w-10 h-10 text-white transition-all bg-orange-500 rounded-full shadow-md hover:bg-orange-400 hover:scale-105 hover:text-orange-700"
+					>
+						<img src="/icons/left-arrow.png" class="w-1/2 h-1/2" alt="" />
+					</div>
+					<div
+						class="flex items-center justify-center w-10 h-10 text-white transition-all bg-orange-500 rounded-full shadow-md hover:bg-orange-400 hover:scale-105 hover:text-orange-700"
+					>
+						<img src="/icons/right-arrow.png" class="w-1/2 h-1/2" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<footer
+	class="flex flex-row gap-4 px-32 py-16 border-t-4 border-gray-100 bg-orange-50 justify-between"
+>
+	<div class="flex-col w-1/3">
+		<h3 class="slackey text-2xl text-black mb-4">Mew</h3>
+		<p class="mb-4">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit nostrum corrupti distinctio
+			id odio temporibus .
+		</p>
+		<div class="flex flex-row gap-8">
+			<div class=" p-2 rounded-full hover:bg-sky-300">
+				<img class="w-10 h-10" src="/svg/facebook.svg" alt="" />
+			</div>
+			<div class=" p-2 rounded-full hover:bg-sky-300">
+				<img class="w-10 h-10" src="/svg/instagram.svg" alt="" />
+			</div>
+		</div>
+	</div>
+	<div class="flex-col text-gray-600">
+		<h3 class="mb-4 text-2xl text-blue-900 slackey">Useful links</h3>
+		<div class="flex flex-col gap-2">
+			<p>
+				<a href="/faqs">FAQs</a>
+			</p>
+			<p>
+				<a href="/shop">Shop</a>
+			</p>
+			<p>
+				<a href="/policy">Privacy Policy</a>
+			</p>
+			<p><a href="/terms">Terms of Use</a></p>
+			<p><a href="/map">Site Map</a></p>
+		</div>
+	</div>
+	<div class="flex-col text-gray-600">
+		<h3 class="mb-4 text-2xl text-blue-900 slackey">Explore</h3>
+		<div class="flex flex-col gap-2">
+			<p>
+				<a href="/about">About us</a>
+			</p>
+			<p><a href="/services">Services</a></p>
+			<p><a href="/team">team</a></p>
+			<p><a href="/adopt">Adopt pet</a></p>
+		</div>
+	</div>
+	<div class="flex-col text-gray-600">
+		<h3 class="mb-4 text-2xl text-blue-900 slackey">Contact info</h3>
+		<div class="flex flex-col gap-2">
+			<p>361-7777-5618</p>
+			<p>654 White Avenure</p>
+			<p>Portland, TX 38784</p>
+		</div>
+	</div>
+</footer> -->
 
 <style>
 	#doghero {
@@ -141,11 +427,16 @@
 
 	#rings-image {
 		position: absolute;
-		bottom: 200px;
-		right: 400px;
+		bottom: 0;
+		right: 0;
 	}
 
 	.slackey {
 		font-family: 'slackey';
+	}
+	.center-image {
+		position: absolute;
+		left: 50%;
+		transform: translate(-50%, 0);
 	}
 </style>
